e88215a46d5754101ecf3d49a6986d5f2dd3d45a
Suah Kang <63537847+rkdtndk99@users.noreply.github.com>
2022-01-12 22:41:44 +0900

    2021.01.12 코테준비기초-그리디

--- Python/BOJ/6064.py
+++ Python/BOJ/6064.py
@@ -1,10 +1,10 @@
 num = int(input())
 for _ in range(num):
     m, n, x, y = map(int, input().split())
-    answer = -1
+    ans = -1
     while x < m*n:
         if(x-y) % n == 0:
-            answer = x
+            ans = x
             break
         x += m
-    print(answer)
+    print(ans)
