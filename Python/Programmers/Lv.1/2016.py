c7db819a1ed1a5fb7f9b5ca29d7d7b8c5faa8acb
Suah Kang <63537847+rkdtndk99@users.noreply.github.com>
2022-02-28 20:48:54 +0900

    programmers level1 - 2016년

--- Python/Programmers/Lv.1/2016.py
+++ Python/Programmers/Lv.1/2016.py
@@ -1,1 +1,14 @@
 # 2016년
+day = ['FRI','SAT','SUN', 'MON','TUE','WED','THU']
+end = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+def solution(a, b):
+    answer = day[(sum(end[:a-1]) + b-1) % 7]
+    return answer
+
+# datetime 라이브러리 사용
+import datetime
+
+def solution(a, b):
+    date = 'MON TUE WED THU FRI SAT SUN'.split()
+    return date[datetime.datetime(2016, a, b).weekday()]
