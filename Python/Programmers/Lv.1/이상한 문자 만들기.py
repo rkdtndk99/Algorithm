aa56b533d21474d7f25171bab71fbd30aa5d4041
Suah Kang <63537847+rkdtndk99@users.noreply.github.com>
2022-03-23 15:16:40 +0900

    2022.03.23 - 프로그래머스 Lv.1

--- Python/Programmers/Lv.1/이상한 문자 만들기.py
+++ Python/Programmers/Lv.1/이상한 문자 만들기.py
@@ -1,1 +1,14 @@
 # 내 풀이
+def solution(s):
+    answer = []
+    words = s.split(" ")
+    for word in words:
+        w = ""
+        for i in range(len(word)):
+            if i % 2:
+                w += word[i].lower()
+            else:
+                w += word[i].upper()
+        answer.append(w)
+
+    return ' '.join(answer)
